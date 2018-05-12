9213ab6b1732a4801f9325d37dcad2927f083cc1
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-05-12 20:03:40 +0200

    Added docblocks

--- engine/Library/Model/Enumerator.php
+++ engine/Library/Model/Enumerator.php
@@ -27,31 +27,49 @@ class Enumerator implements EnumeratorInterface
 		$this->values = $values;
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function model(): ModelInterface
 	{
 		return $this->model;
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function set($id, $value): void
 	{
 		$this->values[$id] = $value;
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function get($id)
 	{
 		return $this->values[$id] ?? null;
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function has($id): bool
 	{
 		return array_key_exists($id, $this->values);
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function unset($id): void
 	{
 		unset($this->values[$id]);
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	public function count(): int
 	{
 		return \count($this->values);
---
+++ engine/Model/Meta/Meta.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jose Cuesta
+ * Date: 12/05/2018
+ * Time: 19:55
+ */
+
+namespace Twist\Model\Meta;
+
+class Meta
+{
+
+}
