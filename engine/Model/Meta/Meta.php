33a45b5d66f4f1e5f7d9923df0d0034b0ffb2909
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-05-12 20:04:10 +0200

    New class for metadata

--- engine/Model/Meta/Meta.php
+++ engine/Model/Meta/Meta.php
@@ -1,14 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jose Cuesta
- * Date: 12/05/2018
- * Time: 19:55
- */
 
 namespace Twist\Model\Meta;
 
-class Meta
+use Twist\Library\Model\Enumerator;
+use Twist\Library\Model\ModelInterface;
+
+/**
+ * Class Meta
+ *
+ * @package Twist\Model\Meta
+ */
+class Meta extends Enumerator
 {
+
+	/**
+	 * Meta constructor.
+	 *
+	 * @param ModelInterface $model
+	 * @param string         $type
+	 */
+	public function __construct(ModelInterface $model, string $type)
+	{
+		parent::__construct($model, get_metadata($type, $model->id()));
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function get($id)
+	{
+		$value = parent::get($id);
+
+		if (!\is_array($value)) {
+			return null;
+		}
+
+		return \count($value) === 1 ? maybe_unserialize($value[0]) : array_map('maybe_unserialize', $value);
+	}
 
 }
