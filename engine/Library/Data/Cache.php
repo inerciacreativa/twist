e2fb34361aa32416215b165ba2e1e23da985ab7d
José Cuesta <jose.cuesta@inerciacreativa.com>
2018-05-20 20:19:47 +0200

    Use only transients

--- engine/Library/Data/Cache.php
+++ engine/Library/Data/Cache.php
@@ -10,57 +10,39 @@ namespace Twist\Library\Data;
 class Cache
 {
 
-    /**
-     * @param string $name
-     * @param mixed  $value
-     * @param int    $expiration
-     */
-    public static function push(string $name, $value, int $expiration = 0)
-    {
-        set_transient($name, $value, $expiration);
-    }
+	/**
+	 * @param string $name
+	 * @param mixed  $value
+	 * @param int    $expiration
+	 *
+	 * @return bool
+	 */
+	public static function set(string $name, $value, int $expiration = 0): bool
+	{
+		return set_transient($name, $value, $expiration);
+	}
 
-    /**
-     * @param string $name
-     *
-     * @return mixed
-     */
-    public static function pull(string $name)
-    {
-        return get_transient($name);
-    }
+	/**
+	 * @param string $name
+	 * @param mixed $default
+	 *
+	 * @return mixed
+	 */
+	public static function get(string $name, $default = false)
+	{
+		$value = get_transient($name);
 
-    /**
-     * @param string    $name
-     * @param mixed     $value
-     * @param null|bool $autoload
-     *
-     * @return bool
-     */
-    public static function set(string $name, $value, bool $autoload = null): bool
-    {
-        return update_option($name, $value, $autoload);
-    }
+		return ($value === false) ? $default : $value;
+	}
 
-    /**
-     * @param string     $name
-     * @param mixed|null $default
-     *
-     * @return mixed
-     */
-    public static function get(string $name, $default = null)
-    {
-        return get_option($name, $default);
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return bool
-     */
-    public static function forget(string $name): bool
-    {
-        return delete_option($name);
-    }
+	/**
+	 * @param string $name
+	 *
+	 * @return bool
+	 */
+	public static function forget(string $name): bool
+	{
+		return delete_transient($name);
+	}
 
 }
