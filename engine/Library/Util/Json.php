d9e50ef6b5e8646497e2ff865d018da6e0ee806b
José Cuesta <jose.cuesta@inerciacreativa.com>
2018-06-20 19:36:11 +0200

    New class to deal with JSON

--- engine/Library/Util/Json.php
+++ engine/Library/Util/Json.php
@@ -1,14 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jose Cuesta
- * Date: 08/06/2018
- * Time: 12:07
- */
 
 namespace Twist\Library\Util;
 
+/**
+ * Class Json
+ *
+ * @package Twist\Library\Util
+ */
 class Json
 {
+
+	/**
+	 * @param mixed $value
+	 * @param int   $options
+	 * @param int   $depth
+	 *
+	 * @return string
+	 */
+	public static function encode($value, int $options = 0, int $depth = 512): string
+	{
+		$json = \json_encode($value, $options, $depth);
+		if (JSON_ERROR_NONE !== json_last_error()) {
+			throw new \InvalidArgumentException('json_encode error: ' . json_last_error_msg());
+		}
+
+		return $json;
+	}
+
+	/**
+	 * @param string $json
+	 * @param bool   $assoc
+	 * @param int    $depth
+	 * @param int    $options
+	 *
+	 * @return array|mixed|object
+	 */
+	public static function decode(string $json, bool $assoc = false, int $depth = 512, int $options = 0)
+	{
+		$data = \json_decode($json, $assoc, $depth, $options);
+		if (JSON_ERROR_NONE !== json_last_error()) {
+			throw new \InvalidArgumentException('json_decode error: ' . json_last_error_msg());
+		}
+
+		return $data;
+	}
 
 }
