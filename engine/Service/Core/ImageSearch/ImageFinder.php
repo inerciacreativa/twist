5e23e837336b7912c73542343b9769d6b4979725
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2018-12-07 07:12:48 +0100

    Code cleaning

--- engine/Service/Core/ImageSearch/ImageFinder.php
+++ engine/Service/Core/ImageSearch/ImageFinder.php
@@ -2,6 +2,7 @@
 
 namespace Twist\Service\Core\ImageSearch;
 
+use Twist\App\AppException;
 use Twist\Library\Util\Url;
 use Twist\Model\Image\Image;
 use Twist\Model\Post\Post;
@@ -31,33 +32,30 @@ class ImageFinder
 	}
 
 	/**
-	 * @param Post $post
+	 * @param Post $parent
 	 *
 	 * @return null|Image
+	 * @throws AppException
 	 */
-	public function get(Post $post): ?Image
+	public function get(Post $parent): ?Image
 	{
 		if (isset($this->image['id']) && $this->image['id'] > 0) {
-			$attach = get_post($this->image['id']);
+			$id = $this->image['id'];
+		} else {
+			$home   = Url::parse(home_url());
+			$source = Url::parse($this->image['src']);
 
-			if (($attach instanceof \WP_Post) && $attach->post_type === 'attachment') {
-				return new Image($attach, $post);
+			if ($source->host === $home->host) {
+				$source->scheme = $home->scheme;
+
+				$id = $this->local($source);
+			} else {
+				$id = $this->external($this->image, $parent);
 			}
 		}
 
-		$home   = Url::parse(home_url());
-		$source = Url::parse($this->image['src']);
-
-		if ($source->host === $home->host) {
-			$source->scheme = $home->scheme;
-
-			$id = $this->local($source);
-		} else {
-			$id = $this->external($post);
-		}
-
 		if ($id) {
-			return new Image($id, $post);
+			return new Image($id, $parent);
 		}
 
 		return null;
@@ -66,52 +64,53 @@ class ImageFinder
 	/**
 	 * @param Url $source
 	 *
-	 * @return bool|int
+	 * @return int
 	 */
-	protected function local(Url $source)
+	protected function local(Url $source): int
 	{
 		global $wpdb;
 
 		$base = wp_upload_dir()['baseurl'] . '/';
 		if (strpos($source, $base) === false) {
-			return false;
+			return 0;
 		}
 
 		$slug = str_replace($base, '', $source);
 		$slug = preg_replace('/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $slug);
 
 		/** @noinspection SqlResolve */
-		$result = $wpdb->get_row($wpdb->prepare("SELECT posts.ID, posts.post_parent FROM $wpdb->posts AS posts, $wpdb->postmeta AS meta WHERE posts.ID = meta.post_id AND meta.meta_key = '_wp_attached_file' AND meta.meta_value = '%s' AND posts.post_type = 'attachment' LIMIT 1", $slug));
+		$result = $wpdb->get_row($wpdb->prepare("SELECT posts.ID FROM $wpdb->posts AS posts, $wpdb->postmeta AS meta WHERE posts.ID = meta.post_id AND meta.meta_key = '_wp_attached_file' AND meta.meta_value = '%s' AND posts.post_type = 'attachment' LIMIT 1", $slug));
 
 		if (!$result) {
-			return false;
+			return 0;
 		}
 
-		return (int) $result->id;
+		return (int) $result->ID;
 	}
 
 	/**
-	 * @param Post $post
+	 * @param array $image
+	 * @param Post  $post
 	 *
-	 * @return bool|int
+	 * @return int
 	 */
-	protected function external(Post $post)
+	protected function external(array $image, Post $post): int
 	{
 		if (!\function_exists('download_url')) {
 			include ABSPATH . 'wp-admin/includes/file.php';
 		}
 
-		if (!preg_match('/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $this->image['src'], $matches)) {
-			return false;
+		if (!preg_match('/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $image['src'], $matches)) {
+			return 0;
 		}
 
 		$temp = [
 			'name'     => basename($matches[0]),
-			'tmp_name' => download_url($this->image['src']),
+			'tmp_name' => download_url($image['src']),
 		];
 
 		if (is_wp_error($temp['tmp_name'])) {
-			return false;
+			return 0;
 		}
 
 		if (!\function_exists('media_handle_sideload')) {
@@ -122,15 +121,15 @@ class ImageFinder
 			include ABSPATH . 'wp-admin/includes/image.php';
 		}
 
-		$id = media_handle_sideload($temp, $post->id(), $this->image['alt']);
+		$id = media_handle_sideload($temp, $post->id(), $image['alt']);
 
 		if (is_wp_error($id)) {
 			@unlink($temp['tmp_name']);
 
-			return false;
+			return 0;
 		}
 
-		return $id;
+		return (int) $id;
 	}
 
 }
