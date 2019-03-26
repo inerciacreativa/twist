bef4a1934834c04196aaa4fbdbc7342420312d27
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2019-03-26 12:09:37 +0100

    Removed unnecessary scripts, renamed route

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,22 +1,17 @@
-/** Import jQuery */
-import $ from 'jquery';
 /** Import local dependencies */
 import Router from './util/Router';
 import common from './routes/common';
 import home from './routes/home';
-import aboutUs from './routes/about';
+import single from './routes/single';
 
 /**
  * Populate Router instance with DOM routes
  * @type {Router} routes - An instance of our router
  */
 const routes = new Router({
-  /** All pages */
   common,
-  /** Home page */
   home,
-  /** About Us page, note the change from about-us to aboutUs. */
-  aboutUs,
+  single,
 });
 
 /** Load Events */
