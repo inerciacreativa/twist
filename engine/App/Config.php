4cc640e0f40fe15e4e2198b8887d60e407b5308a
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2019-04-17 11:22:17 +0200

    Removed unused method

--- engine/App/Config.php
+++ engine/App/Config.php
@@ -13,22 +13,6 @@ use Twist\Library\Util\Data;
 class Config extends Repository
 {
 
-    /**
-     * Load a config file in JSON format.
-     *
-     * @param string $file The name of the config file.
-     *
-     * @return static
-     */
-    public function load(string $file)
-    {
-        if (is_file($file) && ($values = file_get_contents($file))) {
-            $this->fill(json_decode($values, true));
-        }
-
-        return $this;
-    }
-
 	/**
 	 * Make sure that returns a value (and not a closure).
 	 *
