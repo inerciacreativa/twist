de05f06975d5661b4c8700831559f0894ec33ba2
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2022-05-09 16:51:57 +0200

    Added Twig helper functions

--- engine/View/Twig/TwigExtension.php
+++ engine/View/Twig/TwigExtension.php
@@ -6,8 +6,10 @@ use Kint\Kint;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
+use Twist\Asset;
 use Twist\Library\Html\Attributes;
 use Twist\Library\Html\Classes;
+use Twist\Library\Html\Tag;
 use Twist\Library\Support\Str;
 use Twist\Twist;
 
@@ -28,7 +30,7 @@ class TwigExtension extends AbstractExtension
 	}
 
 	/**
-	 * @return array
+	 * @return TwigFilter[]
 	 */
 	public function getFilters(): array
 	{
@@ -49,11 +51,19 @@ class TwigExtension extends AbstractExtension
 	}
 
 	/**
-	 * @return array
+	 * @return TwigFunction[]
 	 */
 	public function getFunctions(): array
 	{
-		$functions = [
+		return array_filter(array_merge($this->getUrlFunctions(), $this->getAssetFunctions(), $this->getTranslationFunctions(), $this->getDebugFunctions()));
+	}
+
+	/**
+	 * @return TwigFunction[]
+	 */
+	protected function getTranslationFunctions(): array
+	{
+		return [
 			new TwigFunction('__', static function (string $string) {
 				$translation = __($string, 'twist');
 
@@ -88,14 +98,171 @@ class TwigExtension extends AbstractExtension
 				return sprintf($translation, $number);
 			}, ['is_safe' => ['html']]),
 		];
+	}
 
-		if (Twist::config('view.debug') && class_exists(Kint::class)) {
+	/**
+	 * @return TwigFunction[]
+	 */
+	protected function getDebugFunctions(): array
+	{
+		$functions = [];
+
+		if (class_exists(Kint::class) && Twist::config('view.debug')) {
 			$functions[] = new TwigFunction('kint', static function ($debug) {
 				Kint::dump($debug);
 			}, ['is_safe' => ['all']]);
 		}
 
 		return $functions;
+	}
+
+	/**
+	 * @return TwigFunction[]
+	 */
+	protected function getUrlFunctions(): array
+	{
+		return [
+			new TwigFunction('url_site', static function (string $path = '/', string $scheme = null) {
+				return site_url($path, $scheme);
+			}, ['is_safe' => ['all']]),
+			new TwigFunction('url_home', static function (string $path = '/') {
+				return home_url($path);
+			}, ['is_safe' => ['all']]),
+			new TwigFunction('url_admin', static function (string $path = '/') {
+				return admin_url($path);
+			}, ['is_safe' => ['all']]),
+			new TwigFunction('url_request', static function () {
+				global $wp;
+
+				return home_url(trailingslashit($wp->request));
+			}, ['is_safe' => ['all']]),
+			new TwigFunction('url_search_query', static function (bool $escape = true) {
+				return get_search_query($escape);
+			}, ['is_safe' => ['all']]),
+			new TwigFunction('url_asset', static function (string $filename, bool $parent = false) {
+				return Asset::url($filename, $parent);
+			}, ['is_safe' => ['all']]),
+		];
+	}
+
+	/**
+	 * @return TwigFunction[]
+	 */
+	protected function getAssetFunctions(): array
+	{
+		return [
+			new TwigFunction('asset_image', [
+				$this,
+				'getAssetImage',
+			], ['is_safe' => ['html']]),
+			new TwigFunction('asset_svg_inline', [
+				$this,
+				'getAssetSvgInline',
+			], ['is_safe' => ['html']]),
+			new TwigFunction('asset_svg_icon', [
+				$this,
+				'getAssetSvgIcon',
+			], ['is_safe' => ['html']]),
+		];
+	}
+
+	/**
+	 * @param string $filename
+	 * @param array  $attributes
+	 * @param bool   $parent
+	 *
+	 * @return string
+	 */
+	protected function getAssetImage(string $filename, array $attributes = [], bool $parent = false): string
+	{
+		$image = Tag::img(['src' => Asset::url($filename, $parent)]);
+		$image->attributes(array_merge([
+			'alt' => '',
+		], $attributes));
+
+		return $image->render();
+	}
+
+	/**
+	 * @param string $path
+	 * @param array  $attributes
+	 *
+	 * @return string
+	 */
+	protected function getAssetSvgInline(string $path, array $attributes = []): string
+	{
+		$file  = Asset::path($path);
+		$image = @file_get_contents($file);
+
+		if (!$image) {
+			return '';
+		}
+
+		if ($svg = Tag::parse($image)) {
+			$svg->attributes()->remove('version');
+			$svg->attributes()->remove('xmlns');
+			$svg->attributes()->remove('xmlns:xlink');
+			$svg->attributes($attributes);
+			$image = $svg->render();
+		}
+
+		return $image;
+	}
+
+	/**
+	 * @param string      $icon
+	 * @param string|null $title
+	 *
+	 * @return string
+	 */
+	protected function getAssetSvgIcon(string $icon, string $title = null): string
+	{
+		$class = "icon-$icon";
+		$svg   = Tag::svg(['class' => "icon $class"]);
+
+		$svg['focusable'] = 'false';
+
+		if ($title) {
+			$id = $this->getAssetSvgIconTitle($class, $title);
+
+			$svg['role']            = 'img';
+			$svg['aria-labelledby'] = $id;
+
+			$svg->content(Tag::title(['id' => $id], $title));
+		} else {
+			$svg['aria-hidden'] = 'true';
+		}
+
+		return $svg->content(Tag::use([
+			'href'        => "#icon-$icon",
+			'aria-hidden' => 'true',
+		]))->render(true);
+	}
+
+	/**
+	 * @param string $class
+	 * @param string $title
+	 *
+	 * @return string
+	 */
+	protected function getAssetSvgIconTitle(string $class, string $title): string
+	{
+		static $titles = [];
+
+		$id = "${class}__title";
+
+		$count = array_reduce($titles, static function (int $count, array $item) use ($id) {
+			if (strpos($item['id'], $id) === 0) {
+				return ++$count;
+			}
+
+			return $count;
+		}, 0);
+
+		$id       .= "-$count";
+		$titles[] = ['id' => $id, 'title' => $title];
+
+		return $id;
 	}
 
 }
